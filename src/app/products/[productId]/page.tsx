1ad07e7ed4ac00408cda2d8c27e2648939b2b8e8
Aashish Katila <aashishkatila@gmail.com>
2024-07-28 12:32:25 +0545

    Metadata generation

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,23 @@
-export default function Products({
-  params,
-}: {
+import { Metadata } from "next";
+
+type ProductProps = {
   params: { productId: string };
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: ProductProps): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iphone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function Products({ params }: ProductProps) {
   return (
     <div>
       <h1>Detail of Product : {params.productId} </h1>
