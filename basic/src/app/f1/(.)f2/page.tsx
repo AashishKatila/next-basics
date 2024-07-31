735e6220f09846148da0ba49bd5305f7b4782639
Aashish Katila <aashishkatila@gmail.com>
2024-07-31 11:55:22 +0545

    Revert "Handling Routing and Dynamic Routing"
    This reverts commit c46f2e66e0bbfe9d1791225733d43c51ee89b09a.

--- basic/src/app/f1/(.)f2/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function InterceptedF2() {
-  return <div>(.) Intercepted F2 Page</div>;
-}
--- basic/src/app/f1/f2/page.tsx
+++
@@ -1,5 +0,0 @@
-import Link from "next/link";
-
-export default function F2() {
-  return <div>F2 Page</div>;
-}
